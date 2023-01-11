32c548fa2134f1893d135a4bd3f0ea0661c236d6
drmovi <drmovi000@gmail.com>
2023-01-11 22:02:45 +0200

    feat: initial commit

---
+++ src/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drmovi\ComposerMicroservice;
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement activate() method.
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement deactivate() method.
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement uninstall() method.
+    }
+}
